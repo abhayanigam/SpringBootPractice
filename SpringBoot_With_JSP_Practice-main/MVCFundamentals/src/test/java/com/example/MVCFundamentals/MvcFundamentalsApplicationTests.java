e91080532b51f23d157992b9c781ad1359d01840
Abhaya Nigam <abhaya.nigam13@gmail.com>
2023-06-22 12:14:36 +0530

    MVC Fundamentals

---
+++ SpringBoot_With_JSP_Practice-main/MVCFundamentals/src/test/java/com/example/MVCFundamentals/MvcFundamentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MVCFundamentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcFundamentalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
