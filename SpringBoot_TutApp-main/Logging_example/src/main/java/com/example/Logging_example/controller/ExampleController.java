ed51632c1f768f81454032de68adbeeb01b179cd
Abhaya Nigam <abhaya.nigam13@gmail.com>
2023-06-22 11:33:45 +0530

    Logging Level example

--- SpringBoot_TutApp-main/Logging_example/src/main/java/com/example/Logging_example/controller/ExampleController.java
+++ SpringBoot_TutApp-main/Logging_example/src/main/java/com/example/Logging_example/controller/ExampleController.java
@@ -1,2 +1,23 @@
-package com.example.Logging_example.controller;public class ExampleController {
+package com.example.Logging_example.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExampleController {
+
+    Logger logger = LoggerFactory.getLogger(ExampleController.class);
+
+    @RequestMapping("/")
+    public String home(){
+//        logger.error("Error info");
+
+//        logger.trace("Home Method accessed");  // --  for log levels
+        logger.info("This is the error");
+        System.out.println("Hello to this program");
+        logger.info("Exiting");
+        return "Hello Spring Page";
+    }
 }
